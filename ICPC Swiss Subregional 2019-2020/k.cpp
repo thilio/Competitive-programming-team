a1db80e37d911b12db9252eb424e2e34c8c11244
Thiago Lima Oliveira <thilio@usp.br>
2021-02-16 21:20:17 -0300

    Swiss subreg

---
+++ ICPC Swiss Subregional 2019-2020/k.cpp
@@ -0,0 +1,46 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define fst first
+#define snd second
+
+#define fr(i,n) 	for(int i=0;i<n;i++)
+#define frr(i,n)	for(int i=1;i<=n;i++)
+
+#define ms(x,i)	memset(x,i,sizeof(x))
+#define dbg(x)	cout << #x << " = " << x << endl
+#define all(x)	x.begin(),x.end()
+
+#define gnl cout << endl
+#define olar cout << "olar" << endl
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+typedef long long int ll;
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+typedef vector<pii> vii;
+typedef pair<ll,ll> pll;
+
+const int INF = 0x3f3f3f3f;
+const ll llINF = 0x3f3f3f3f3f3f3f;
+const int MOD = 1e9+7;
+
+int main(){
+
+	fastio;
+	ll n,a,b;
+	cin >> n >> a >> b;
+
+	ll mn = 2000;
+
+	for(ll i =0; i <= n; i++){
+		for(ll j = 0; j <= n; j++){
+			if((i*a + j*b)%n == 0 && i + j > 0) mn = min (mn, i + j);
+		}
+	}
+
+	cout << mn << endl;
+
+}
