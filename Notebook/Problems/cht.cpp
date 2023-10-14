183d94d4b441bb127decadb88b2597788b5c5965
Lucas Harada <lucas.harada1900@gmail.com>
2023-10-13 22:51:35 -0300

    Added cht that can support angular coefficents in any order

--- Notebook/Problems/cht.cpp
+++ Notebook/Problems/cht.cpp
@@ -54,3 +54,48 @@ void build(){
 	for(int i = 1; i <= n; i++)
 		c.insert(l[i].f, l[i].s);
 }
+
+/*
+	Title: Convex Hull Trick
+
+	Description: DP optimization from O(n^2) to O(n log n)
+		It receives a set of lines and takes the maximum in a given point x
+
+	Complexity:
+		- insert: O(log n)
+		- query: O(log n)
+
+	Details: The angular coeficient can be inserted in any order
+*/
+
+bool Q;
+struct Line {
+	mutable ll k, m, p;
+	bool operator<(const Line& o) const {
+		return Q ? p < o.p : k < o.k;
+	}
+};
+struct Cht : multiset<Line> {
+	// (for doubles, use inf = 1/.0, div(a,b) = a/b)
+	const ll inf = INT_MAX;
+	ll div(ll a, ll b) { // floored division
+		return a / b - ((a ^ b) < 0 && a % b); }
+	bool isect(iterator x, iterator y) {
+		if (y == end()) { x->p = inf; return false; }
+		if (x->k == y->k) x->p = x->m > y->m ? inf : -inf;
+		else x->p = div(y->m - x->m, x->k - y->k);
+		return x->p >= y->p;
+	}
+	void add(ll k, ll m) { // k * m + x and MAX CHT
+		auto z = insert({k, m, 0}), y = z++, x = y;
+		while (isect(y, z)) z = erase(z);
+		if (x != begin() && isect(--x, y)) isect(x, y = erase(y));
+		while ((y = x) != begin() && (--x)->p >= y->p)
+			isect(x, erase(y));
+	}
+	ll query(ll x) {
+		assert(!empty());
+		Q = 1; auto l = *lower_bound({0,0,x}); Q = 0;
+		return l.k * x + l.m;
+	}
+};
