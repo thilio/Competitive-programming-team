29721182df6e67bb66991491b3764e9538872955
gabrielmorete <gmorete7@gmail.com>
2024-04-06 15:39:23 -0300

    tarjan scc

--- WF_notebook/Graphs/tarjan(scc).cpp
+++ WF_notebook/Graphs/tarjan(scc).cpp
@@ -1,24 +1,12 @@
-/*
-   Title: Tarjan's algorithm for strong connected components (scc)
-   Description: Build the strong connected condensation graph
-   Complexity:  O(E + V)
-   
-   Details: Vertices u and v are in the same component if u -> v and v -> u
-
-*/
-
-const int MAXN = 1e5 + 10; // number of vertices
-
+/*Build the strong connected condensation digraph O(E + V)*/
 int n, clk, sn, id;
-int pre[MAXN], lo[MAXN], stk[MAXN], scc[MAXN]; // scc[v] is the strong component of v
+int pre[MAXN], lo[MAXN], stk[MAXN], scc[MAXN];
 vector<int> adj[MAXN], adjscc[MAXN];
-
 void dfs_scc(int v){
 	pre[v] = lo[v] = clk++;
 	stk[sn++] = v;
 	for (auto x : adj[v]){
-		if (pre[x] == -1)
-			dfs_scc(x);
+		if (pre[x] == -1) dfs_scc(x);
 		lo[v] = min(lo[v], lo[x]);
 	}
 	if (lo[v] == pre[v]){
@@ -31,7 +19,6 @@ void dfs_scc(int v){
 		id++;
 	}
 }
-
 int findscc(){
 	fill(pre, pre + n + 1, -1);
 	clk = sn = id = 0;
@@ -39,11 +26,4 @@ int findscc(){
 		if (pre[v] == -1)
 			dfs_scc(v);
 	return id; // number of strong components
-}
-
-void build_scc_graph(){ // Build condensation dag
-	for (int v = 0; v < n; v++)
-		for (auto x : adj[v])
-			if (scc[v] != scc[x])
-				adjscc[scc[v]].push_back(scc[x]);
 }
