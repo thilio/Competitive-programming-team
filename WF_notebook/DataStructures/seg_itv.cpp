0198a9a915dd99f666e31ddfe9c9dd8c0829f345
gabrielmorete <gmorete7@gmail.com>
2024-04-06 22:30:43 -0300

    seg itv

--- WF_notebook/DataStructures/seg_itv.cpp
+++ WF_notebook/DataStructures/seg_itv.cpp
@@ -1,13 +1,12 @@
 //It is ZERO indexed, just make a dummy zero.
 namespace seg{
 	coord N, t[2 * MAXN];
-
 	void init(int n){ // Index on range [0, N)
 		N = n;
 		fill(t, t + 2 * (n + 1), NEUT);
 	}
-	void modify(int p, coord value){  // set value at position p
-		for (t[p += N] = value; p > 1; p >>= 1) // One may change here to modify a value
+	void modify(int p, coord value){ //set value at p
+		for (t[p += N] = value; p > 1; p >>= 1)
 			t[p>>1] = opr(t[p], t[p^1]);
 	}
 	coord query(int l, int r) {  // sum on interval [l, r)
