b261378c7c9af79976ce7851216de6a726a9cebc
gabrielmorete <gmorete7@gmail.com>
2024-04-06 22:18:06 -0300

    persistent seg

--- WF_notebook/DataStructures/seg_persistent.cpp
+++ WF_notebook/DataStructures/seg_persistent.cpp
@@ -1,44 +1,43 @@
+/* build O(nlog(n)) | update O(log(n)) | query O(log(n))*/
 namespace seg{
 	vector<int> seg(1, NEUT), lft(1, 0), rgt(1, 0); 
 	int rt(0); // last root 
-	void init(){ seg = vector<int>(1, NEUT); lft = vector<int>(1, 0); rgt = vector<int>(1, 0); rt = 0;}
+	void init(){
+		seg = vector<int>(1, NEUT); 
+		lft = vector<int>(1, 0); 
+		rgt = vector<int>(1, 0); rt = 0;
+	}
 	int new_node(int v, int l = 0, int r = 0){
-		seg.pb(v);
-		lft.pb(l);
-		rgt.pb(r);
+		seg.pb(v); lft.pb(l); rgt.pb(r);
 		return (int)(seg.size()) - 1;
 	}
-	int build(int l, int r, int *a){ // put all elements of array on new root rt
-		if(l == r)
-			return new_node(a[l]);
-		
+	// put all elements of array on new root rt
+	int build(int l, int r, int *a){ 
+		if(l == r) return new_node(a[l]);
 		int m = (l + r)/2;
-		int nl = build(l, m, a);
-		int nr = build(m + 1, r, a);
-		return rt = new_node(oper(seg[nl], seg[nr]), nl, nr);
+		int nl = build(l, m, a), nr = build(m + 1, r, a);
+		return rt =new_node(oper(seg[nl], seg[nr]), nl, nr);
 	}
-	int update(int nd, int l, int r, int pos, int val){ // returns the new root
+	// returns the new root
+	int update(int nd, int l, int r, int pos, int val){
 		int ks = new_node(seg[nd], lft[nd], rgt[nd]);
-		if (l == r){
-			seg[ks] = val;
-			return ks;
-		}
+		if (l == r){ seg[ks] = val; return ks; }
 		int m = (l + r)/2, ps;
-		if(pos <= m)
-			ps = update(lft[nd], l, m, pos, val), lft[ks] = ps; // DO NOT change this
-		else 
-			ps = update(rgt[nd], m + 1, r, pos, val), rgt[ks] = ps;
-		
+		if(pos <= m) // DO NOT change this!!!!!!!!!!
+			ps = update(lft[nd],l,m,pos,val), lft[ks] = ps; 
+		else // DO NOT change this!!!!!!!!!!
+			ps = update(rgt[nd],m+1,r,pos,val), rgt[ks]= ps;
 		seg[ks] = oper(seg[lft[ks]], seg[rgt[ks]]);
 		return ks;
 	}
 	int query(int nd, int l, int r, int ql, int qr){
-		if(r < ql or qr < l)
-			return NEUT;
-		if(ql <= l and r <= qr)
-			return seg[nd];
+		if(r < ql or qr < l) return NEUT;
+		if(ql <= l and r <= qr) return seg[nd];
 		int m = (l + r)/2;
-		return oper(query(lft[nd], l, m, ql, qr), query(rgt[nd], m + 1, r, ql, qr));
+		return oper(query(lft[nd], l, m, ql, qr), 
+						query(rgt[nd], m + 1, r, ql, qr));
 	}
-	int update(int l, int r, int pos, int val){ return rt = update(rt, l, r, pos, val); } // on last root
+	int update(int l, int r, int pos, int val){ // last root
+		return rt = update(rt, l, r, pos, val); 
+	}
 } 
