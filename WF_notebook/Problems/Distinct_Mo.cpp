cbb1fa45131871a957b99de3e37b1e0f8fea298e
Thiago Lima Oliveira <thilio@ime.usp.br>
2024-04-09 02:16:44 -0300

    Change MO

--- WF_notebook/Problems/Distinct_Mo.cpp
+++ WF_notebook/Problems/Distinct_Mo.cpp
@@ -1,15 +1,12 @@
 /*
-   Title: Mo's algorithm
-   Description: Mo's algorithm to solve number of distinct elements in range
-   Complexity: O(n + q sqrt(n))
-   Details: Mo's algorithm can be used when queries are offline and
-   			it is easy to insert and erase one element in the answer
-   Solves: https://www.spoj.com/problems/DQUERY/
+Description:number of distinct elements in range
+Complexity: O(n + q sqrt(n))
+Details: Mo's algorithm can be used when queries are
+offline and it is easy to insert and erase one element
+in the answer. Another example is dynamic connectivity
+when you can add edges easily ,say DSU, and remove easily
+with rollback
 */
-
-#include "bits/stdc++.h"
-using namespace std;
-
 vector<int> v, resp;
 int freq[MAXA];
 
@@ -47,42 +44,27 @@ int query(int x, int y){
 }
  
 int main(){
-
 	int n, q;
- 
 	cin >> n;
 	v.resize(n);
-
 	sq = (int)ceil(sqrt(n));
-
 	for(int i = 0; i < n; i++){
 		cin >> v[i];
 	}
-
 	cin >> q;
 	vector<qry> Q;
-
 	resp.resize(q);
-
 	for(int i = 0; i < q; i++){
 		int a, b;
 		cin >> a >> b;
 		a--; b--;
-
 		Q.push_back({a, b, i});
 	}
-
 	sort(Q.begin(), Q.end());
-
 	for(auto x: Q){
 		resp[x.ind] = query(x.l, x.r);
 	}
-
 	for(auto x: resp){
 		cout << x << endl;
 	}
- 
- 
- 
- 
 }
